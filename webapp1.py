b633bb3f99ebb75c31d1df19e06d37346400038c
MarcAnthony Fanfan <mfanx2@gmail.com>
2019-10-23 15:16:56 -0400

    Add single quotes to sql

--- webapp1.py
+++ webapp1.py
@@ -63,10 +63,10 @@ def create_app(test_config=None):
             username = details['username']
             password = details['password']
             cur1 = mysql.connection.cursor()
-            cur1.execute("SELECT * FROM users WHERE username=%s", [username])
+            cur1.execute("SELECT * FROM users WHERE username='%s'", [username])
             if cur1.rowcount == 0:
                 cur2 = mysql.connection.cursor()
-                cur2.execute("INSERT INTO users(username, password) VALUES (%s, %s)", (username, password))
+                cur2.execute("INSERT INTO users(username, password) VALUES ('%s', '%s')", (username, password))
                 response = make_response(redirect('/dashboard'))
                 response.set_cookie('username', username)
             else:
