14f28e925effe0c56b879b3773d0762bfb851641
MarcAnthony Fanfan <mfanx2@gmail.com>
2019-10-25 13:23:04 -0400

    Move export FLASK_ENV to start script

--- start.sh
+++ start.sh
@@ -3,4 +3,8 @@
 cd /webapp1
 git reset --hard HEAD
 git pull
+case $HOSTNAME in
+  (u1910-dev) export FLASK_ENV=development;;
+  (u1910-prod) export FLASK_ENV=production;;
+esac
 flask run --cert=/cert.pem --key=/key.pem --host 0.0.0.0
