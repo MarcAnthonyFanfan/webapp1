da72beb7c99a265841bdd6b18b1a7247666ed699
MarcAnthony Fanfan <mfanx2@gmail.com>
2019-10-25 13:33:52 -0400

    Troubleshooting FLASK_ENV

--- start.sh
+++ start.sh
@@ -3,9 +3,13 @@
 cd /webapp1
 git reset --hard HEAD
 git pull
-if [ "$HOSTNAME" = u1910-dev ]; then
+echo "BEFORE SETTING ENV:"
+echo $FLASK_ENV
+if [ "$HOSTNAME" = "u1910-dev" ]; then
     export FLASK_ENV=development
 else
     export FLASK_ENV=production
 fi
+echo "AFTER SETTING ENV"
+echo $FLASK_ENV
 flask run --cert=/cert.pem --key=/key.pem --host 0.0.0.0
