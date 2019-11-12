512f80823ff14c6c1dfcbc390f872ba2ad4b51fa
MarcAnthony Fanfan <mfanx2@gmail.com>
2019-11-12 12:57:47 -0500

    Add db to db_reset script

--- db_reset.sh
+++ db_reset.sh
@@ -4,7 +4,7 @@ if [[ $prompt == "y" || $prompt == "Y" || $prompt == "yes" || $prompt == "Yes" ]
 then
     sudo mysql --execute="DROP DATABASE IF EXISTS flaskapp;"
     sudo mysql < db_setup.sql
-    sudo mysql --execute="INSERT INTO users (email, username, password, is_admin) values ('mfanx2@gmail.com', 'mfanx2', '088409e872857b396b89ab899245c7a4', 1);"
+    sudo mysql --database="flaskapp" --execute="INSERT INTO users (email, username, password, is_admin) values ('mfanx2@gmail.com', 'mfanx2', '088409e872857b396b89ab899245c7a4', 1);"
     sudo mysql --execute="INSERT INTO users (email, username, password, is_admin) values ('test_admin@selenium.test', 'test_admin', 'a08099eb3efa177a74bee07ee30552a1', 1)"
     echo "flaskapp db reset"
 else
