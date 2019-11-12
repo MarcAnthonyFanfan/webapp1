8006a7d5d00a2dfa86dd111c6fcf8866d5d8e32f
MarcAnthony Fanfan <mfanx2@gmail.com>
2019-11-12 15:49:03 -0500

    Change exit codes on db_reset.sh

--- db_reset.sh
+++ db_reset.sh
@@ -7,7 +7,8 @@ then
     sudo mysql --database="flaskapp" --execute="INSERT INTO users (email, username, password, is_admin) values ('mfanx2@gmail.com', 'mfanx2', '088409e872857b396b89ab899245c7a4', 1);"
     sudo mysql --database="flaskapp" --execute="INSERT INTO users (email, username, password, is_admin) values ('test_admin@selenium.test', 'test_admin', 'a08099eb3efa177a74bee07ee30552a1', 1)"
     echo "flaskapp db reset"
+    exit 0
 else
     echo "flaskapp db not reset"
-    exit 0
+    exit 1
 fi
